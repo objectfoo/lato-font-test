35e36d2e2d5ee5915c9d4aec4272d449ca755774
objectfoo <objectfoo@users.noreply.github.com>
2025-09-09 12:15:46 -0700

    use google for v24, add v13, update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+	build: {
+		rollupOptions: {
+			input: {
+				main: "index.html",
+				"lato-google-latin-v13": "lato-google-latin-v13.html",
+				"lato-google-latin-v24": "lato-google-latin-v24.html",
+				"lato-latin-v2": "lato-latin-v2.html",
+			}
+		}
+	}
+});
