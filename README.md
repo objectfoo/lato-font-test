e31351ab0ecf08f5a95911943ab61b7dd77c0a58
Satake <objectfoo@users.noreply.github.com>
2025-09-08 22:56:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+create some test pages for fonts
+
+* google lato v13
+* google lato v24
+* lato-lato
+
+* [lato-lato](https://www.latofonts.com/lato-free-fonts/)
+* [goog-lato-24]()
+
+
+## google lato 24
+
+font weights: 100, 300, 400, 700, 900
+
+https://fonts.google.com/specimen/Lato
+
+
+## Lato-lato v2
+
+font weights: 100, 200, 300, 400, 500, 600, 700, 800, 900
+
+https://fonts.adobe.com/fonts/lato
+
+## Fonts assumed from @webmdhs/mui-theme (and usage)
+
+font weights: 300, 400, 600, 700, (900)
+
+
+## current phm weights
+
+font weights: 300, 400, 700, 900
+
+
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+	return (
+		<div style={{ maxWidth: 600 }}>
+			<h1>Interdum et malesuada</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper, justo eget vehicula vestibulum, enim enim suscipit lectus, et sagittis nibh risus vel metus. Quisque eu ornare ante, et gravida mauris. Vivamus massa justo, sagittis non viverra sed, sodales non nisi. Nunc semper, massa a aliquet dictum, enim nisi malesuada orci, et elementum lectus turpis et velit. Nam vel felis vitae tortor dignissim malesuada. Nam suscipit, justo eu elementum pulvinar, magna sem tempor ex, vitae iaculis tellus odio non nisl. Duis dolor orci, viverra ut finibus sed, aliquet vitae tortor. Proin sodales ipsum ac ipsum hendrerit tempus. Nunc nec nibh nibh. Aenean consequat auctor posuere. Integer sed magna volutpat, efficitur nisl ut, dignissim neque. Vestibulum convallis nec dui a euismod. Duis dignissim magna in mattis pulvinar. Sed blandit nibh quis arcu ornare, sit amet fermentum nisi rhoncus.
+			</p>
+			<h2>Sed consectetur tellus</h2>
+			<p>
+				Vestibulum lobortis ultricies ipsum, a maximus ligula dignissim in. Sed consectetur tellus egestas, consequat dolor at, tempus augue. Morbi quis ipsum quis velit varius laoreet in scelerisque erat. Suspendisse sed accumsan erat. Proin sagittis ultricies orci id pellentesque. Fusce iaculis mauris quis pulvinar lobortis. Donec sit amet porttitor lorem, vel scelerisque justo.
+			</p>
+			<h2>Lorem ipsum dolor sit</h2>
+			<p>
+				Interdum et malesuada fames ac ante ipsum primis in faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a iaculis magna, vitae lacinia metus. Etiam at lobortis ligula. Ut libero arcu, tincidunt in sodales nec, sodales vitae neque. Donec condimentum, est ut viverra efficitur, diam sem tempor lacus, a auctor mi nibh vehicula neque. Morbi porttitor velit elit, hendrerit aliquam eros efficitur nec. Pellentesque pretium, erat eget vehicula molestie, lorem elit mattis neque, vel volutpat mi lacus sit amet quam. Quisque in malesuada tortor. Aliquam erat volutpat. Quisque nisl risus, elementum non lorem in, ultrices aliquet est. Nullam luctus finibus quam ut tempus. Nulla facilisi.
+			</p>
+		</div>
+	);
+}
+
+export default App;
