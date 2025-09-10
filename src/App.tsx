67e4f3fc7add2ac204aeaae6f8b8b8e5829b8352
Andrew Kington <objectfoo@users.noreply.github.com>
2025-09-10 12:58:53 -0700

    add change password text from issue to sample

--- src/App.tsx
+++ src/App.tsx
@@ -64,6 +64,9 @@ function SampleBlock(props: { size: number, weight: number; }): React.ReactNode 
 
 			<p style={sizeWeight} className={`italic`}>a b c d e f g h i j k l m n o p q r s t u v w x y z</p>
 			<div style={{ marginTop: 16, ...sizeWeight }}>
+				<div style={{ fontSize: 36, letterSpacing: "-.13px", lineHeight: 1.2, ...sizeWeight }}>Şifrenizi Güncelleyin</div>
+			</div>
+			<div style={{ marginTop: 16, ...sizeWeight }}>
 				<p>Sezgin Ataç (born 21 March 1998)[1] is a Turkish long-distance runner. He competed in the men's race at the 2020 World Athletics Half Marathon Championships held in Gdynia, Poland.[2]</p>
 				<p>In 2019, he competed in the men's event at the 2019 European 10,000m Cup held in London, United Kingdom. In the same year, he finished in 17th place in the men's 10,000 metres event at the 2019 European Athletics U23 Championships held in Gävle, Sweden.[3]</p>
 			</div>
