48dfe661bf840a69903df5ba8ed1d934842900f9
Andrew Kington <objectfoo@users.noreply.github.com>
2025-09-11 11:55:09 -0700

    redo index make a real page for lato 13, add some problem text to sample

---
+++ src/AppMaterial.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import { SampleView } from "./SampleView";
+
+export function AppMaterial() {
+	return <SampleView />;
+}
+
+
---
+++ src/AppVanilla.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+import { SampleView } from "./SampleView";
+
+export function AppVanilla() {
+	return <SampleView />;
+}
+
